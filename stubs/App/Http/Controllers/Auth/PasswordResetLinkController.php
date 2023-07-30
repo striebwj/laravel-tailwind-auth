5bc9b81bc1ef061ad8023376a623a081584f4438
Wade Striebel <wade@striebel.ca>
2023-07-29 22:56:30 -0400

    fix: double import of request

--- stubs/App/Http/Controllers/Auth/PasswordResetLinkController.php
+++ stubs/App/Http/Controllers/Auth/PasswordResetLinkController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers\Auth;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\JsonResponse;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Password;
 use Illuminate\Validation\ValidationException;
 
